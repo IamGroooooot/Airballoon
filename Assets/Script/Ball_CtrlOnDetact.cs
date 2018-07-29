c4e0520959a355646f8624ea8cf1bef2c2e3b36a
JUHYEONG-PC\duryk <dury.ko@gmail.com>
2018-07-29 18:27:17 +0900

    SkelBomb구현 완료!

--- Assets/Script/Ball_CtrlOnDetact.cs
+++ Assets/Script/Ball_CtrlOnDetact.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 //Ballon Ship에 대한 스크립트 : 폭탄 투하하게 함
 
-public class Bal_Ship : MonoBehaviour {
+public class Ball_CtrlOnDetact : MonoBehaviour {
 
-	public Transform PlayerTrans;
-    public GameObject Skel;
+	public Transform PlayerTrans; //플레이어
+    public GameObject Skel; //스켈레톤이 있는 열기구 설정
 
-	public GameObject bomb;
-
-//	private Transform MyTrans;
-//	private Rigidbody MyRigid;
+	public GameObject bomb; //발사할 스켈레톤의 폭탄 prefab
+    
 	private int time;
-	private Vector3 distance;
 	public bool canAttack,fire,TimerOn;
 
 	public float speed = 5f;
     private Vector3 myPosition;
     private Quaternion myRotation;
+
+    //해골이 던지는 폭탄에 대한 설정 값
+    public float angle_degr = 45f; //기본각도를 45도로 설정0.0174532924
+    public float bombSpeed = 1f;
+    private GameObject Skel_Bomb;
+    Vector3 BombInitVelocity;
+    Vector3 displace;
+    float distance;
+    Vector3 playerPos;
+
 
     //	public GameObject currentHitObject;
     //
@@ -42,7 +49,8 @@ public class Bal_Ship : MonoBehaviour {
         myPosition = new Vector3(0, 0, 0);
         myRotation = new Quaternion(0, 0, 0, 0);
 		time = 0;
-	}
+        angle_degr = angle_degr * Mathf.Deg2Rad;// Deg2Rad
+    }
 	
 	// Update is called once per frame
 	void Update () { 
@@ -61,7 +69,6 @@ public class Bal_Ship : MonoBehaviour {
 			Quaternion rotation = Quaternion.LookRotation (newDir);
 			//transform.rotation = Quaternion.LookRotation (newDir);
 
-;
             Skel.transform.rotation = Quaternion.Lerp (Skel.transform.rotation, rotation, step);
 
 			float MyAngle = Skel.transform.eulerAngles.y;
@@ -72,27 +79,8 @@ public class Bal_Ship : MonoBehaviour {
 			if (Mathf.Abs (RotAngle) < 110 && Mathf.Abs (RotAngle) > 89) {
 				TimerOn = true;
 			}
-//			if (Mathf.Abs(RotAngle) > 180)
-//			{
-//				if (RotAngle > 0)
-//				{ //270
-//					RotAngle = RotAngle - 360;
-//				}
-//				else if (RotAngle < 0)
-//				{
-//					RotAngle = RotAngle + 360;
-//				}
-//			}
-//			if (RotAngle > 1) {
-//				//MyRigid.AddTorque (0, Mathf.Clamp (1.5f + 5 * RotAngle / 180f, 0, 1), 0);
-//			} else if (RotAngle < -1) {
-//				//MyRigid.AddTorque (0, Mathf.Clamp (-1.5f + 5 * RotAngle / 180f, -1, 0), 0);
-//			} else {
-//				MyRigid.angularVelocity = new Vector3 (0, 0, 0);
-//				TimerOn = true;
-//			}
-
 		}
+
 		if (TimerOn) {
 			
 			time++;
@@ -103,7 +91,8 @@ public class Bal_Ship : MonoBehaviour {
 		}
 		if (fire) {
 			//인스턴트화하고 폭탄 스크립트에 폭탄 발사 넣기
-			Instantiate(bomb, Skel.transform.position + (Skel.transform.forward*20),Skel.transform.rotation);
+			Skel_Bomb = Instantiate(bomb, Skel.transform.position + (Skel.transform.forward*20) ,Skel.transform.rotation) as GameObject;
+            FireSkelBomb(Skel_Bomb);
 			Debug.Log ("인스턴스화 완료");
             time = 0;
 			fire = false;
@@ -132,7 +121,6 @@ public class Bal_Ship : MonoBehaviour {
 	void OnTriggerStay(Collider C){
 		if(C.gameObject.tag == "Player")
 			canAttack = true;
-
 	}
 
 	void OnTriggerExit(Collider C){
@@ -141,4 +129,31 @@ public class Bal_Ship : MonoBehaviour {
 			TimerOn = false;
 		}
 	}
+
+    void FireSkelBomb(GameObject Bomb)
+    {
+        
+        //GameObject player = GameObject.FindGameObjectWithTag("Player");
+        //Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y + 33f, player.transform.position.z);
+        playerPos = new Vector3(PlayerTrans.position.x, PlayerTrans.position.y, PlayerTrans.position.z); //보정값
+        //안바뀜
+        displace = playerPos - transform.position;
+        //안바뀜
+        distance = Mathf.Pow((displace.x) * (displace.x) + (displace.z) * (displace.z), 0.5f);
+        //아래에서 오류발생
+        BombInitVelocity = new Vector3(displace.x, distance * Mathf.Tan(angle_degr), displace.z).normalized;
+        Debug.Log("Nomal Vector체크 " + BombInitVelocity);
+        //위에서 오류 발생, y축 속도가 누락?됨
+        
+
+        BombInitVelocity = BombInitVelocity * Mathf.Sqrt( Mathf.Abs(Physics.gravity.y)*distance/Mathf.Cos(angle_degr) );
+
+        //Debug.Log("변위벡터는 " + displace);
+        //Debug.Log("거리는 " + distance);
+        Debug.Log("최종 초기 속력 값은 ? " + BombInitVelocity);
+        Bomb.GetComponent<Rigidbody>().velocity = BombInitVelocity * bombSpeed;//강체를 찾을 수 없음?????
+
+        Destroy(Bomb.gameObject, 5f);
+    }
+
 }
--- Assets/Script/Skel_Bomb.cs
+++ Assets/Script/Skel_Bomb.cs
@@ -4,34 +4,52 @@ using UnityEngine;
 
 
 
-public class Skel_Bomb : MonoBehaviour {
-
-    public float angle = 45f * Mathf.Deg2Rad; //기본각도를 45도로 설정0.0174532924
-    public float bombSpeed = 10f;
-
+public class Skel_Bomb : MonoBehaviour
+{
     // Use this for initialization
-    void Start () {
-		GameObject player = GameObject.FindGameObjectWithTag ("Player");
-        Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y+33f, player.transform.position.z);
-        Vector3 position = playerPos - transform.position;
-        Vector3 BombInitVelocity = new Vector3(position.x, Mathf.Pow(Mathf.Sqrt(position.x) + Mathf.Sqrt(position.z), 0.5f) * Mathf.Tan(angle), position.z) ;
-        BombInitVelocity = BombInitVelocity.normalized * Mathf.Pow(((Mathf.Sqrt(position.x) + Mathf.Sqrt(position.z)) *Mathf.Abs(Physics.gravity.y))/Mathf.Sin(2*angle), 0.5f);
+    void Start()
+    {
+        //angle_degr = angle_degr *Mathf.Deg2Rad; // Deg2Rad
+        //GameObject player = GameObject.FindGameObjectWithTag ("Player");
+        //Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y+33f, player.transform.position.z);
+        //Vector3 position = playerPos - transform.position;
+        //Vector3 BombInitVelocity = new Vector3(position.x, Mathf.Pow(Mathf.Sqrt(position.x) + Mathf.Sqrt(position.z), 0.5f) * Mathf.Tan(angle_degr), position.z) ;
+        //BombInitVelocity = BombInitVelocity.normalized * Mathf.Pow(((Mathf.Sqrt(position.x) + Mathf.Sqrt(position.z)) *Mathf.Abs(Physics.gravity.y))/Mathf.Sin(2*angle_degr), 0.5f);
 
-        Debug.Log(Mathf.Pow(((Mathf.Sqrt(position.x) + Mathf.Sqrt(position.z)) * Mathf.Abs(Physics.gravity.y)) / Mathf.Sin(2 * angle), 0.5f));
-        GetComponent<Rigidbody> ().velocity = BombInitVelocity*bombSpeed;
-        Destroy(this.gameObject,5f);
+        //Debug.Log(Mathf.Pow(((Mathf.Sqrt(position.x) + Mathf.Sqrt(position.z)) * Mathf.Abs(Physics.gravity.y)) / Mathf.Sin(2 * angle_degr), 0.5f));
+        //GetComponent<Rigidbody> ().velocity = BombInitVelocity*bombSpeed;//강체를 찾을 수 없음?????
+        //Destroy(this.gameObject,5f);
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-	void OnCollisionEnter (Collision C){
-		if (C.gameObject.tag == "Player") {
-			C.gameObject.GetComponent<HP_Bar> ().IsDamaged = true;
-			Destroy (this.gameObject);
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
+    }
+
+    //void OnCollisionEnter(Collision C)
+    //{
+    //    if (C.gameObject.tag == "Player")
+    //    {
+    //        C.gameObject.GetComponent<HP_Bar>().IsDamaged = true;
+    //        Destroy(this.gameObject);
+    //    }
+    //}
+
+
+    void OnTriggerEnter(Collider Coll)
+    {
+        if (Coll.gameObject.tag == "Player")
+        {
+            Coll.gameObject.GetComponent<HP_Bar>().IsDamaged = true;
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerStay(Collider CollStay)
+    {
+        OnTriggerEnter(CollStay);
+    }
 }
+
+
