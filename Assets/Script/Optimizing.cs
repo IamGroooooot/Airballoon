e0765b8ceac80da357682d4610ca1cb5e68910f3
Iamgroooooot <dury.ko@gmail.com>
2018-08-07 01:01:32 +0900

    벛꽃추가

---
+++ Assets/Script/Optimizing.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Optimizing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnBecameInvisible()
+
+	{
+		Off ();
+	}
+
+	void OnBecameVisible()
+
+	{
+		ON ();
+	}
+
+	void Off()
+	{
+		this.gameObject.SetActive (false);		
+	}
+		
+	void ON()
+	{
+		this.gameObject.SetActive (true);		
+	}
+}
