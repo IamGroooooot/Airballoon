b40e7fcf2f175d895b3f5f9738ab3da827f9e078
Iamgroooooot <dury.ko@gmail.com>
2018-08-07 03:33:40 +0900

    안드로이드 Path 인식 못해서Resources.Load 사용함. 그 과정에서 json파일을 text로 바꿈 (json파일을 textasset으로 인식이 안됨...)

--- Assets/Script/Inventory/Item_Database.cs
+++ Assets/Script/Inventory/Item_Database.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using LitJson;
 using System.IO;
+
 public class Item_Database : MonoBehaviour {
 	private List<Item> database = new List<Item>();
 	private JsonData itemData;
+	TextAsset file;
 
 	void Start()
 	{
+		file = Resources.Load<TextAsset>("Items");
+		string jsonString = file.ToString();
 
-		itemData = JsonMapper.ToObject (File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+		itemData = JsonMapper.ToObject (jsonString);
+
+			
 		ConstructItemDatabase ();
 
 	}
