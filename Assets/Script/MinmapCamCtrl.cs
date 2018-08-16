055e9edb182456510cf29b8ce0d878e33e494c7c
Iamgroooooot <dury.ko@gmail.com>
2018-08-16 14:34:04 +0900

    MiniMap Fixed 구현 완료

---
+++ Assets/Script/MinmapCamCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinmapCamCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 newPos =PlayerManager.instance.player.transform.position;
+		GetComponent<Transform> ().position =  new Vector3(newPos.x,newPos.y+3000,newPos.z);
+		transform.eulerAngles = new Vector3 (90, 0, 0);
+	}
+
+}
